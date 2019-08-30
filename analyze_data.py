50e0480c71d1d51c9ae03b30eea1cf1f36e60576
JoeMulka <msutennis23@gmail.com>
2019-08-29 23:56:22 -0400

    using matplotlib to plot time data

--- analyze_data.py
+++ analyze_data.py
@@ -1,2 +1,32 @@
+import matplotlib.pyplot as plt
 import matplotlib
 import csv
+import argparse
+import datetime
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("datafile", help = "the file containing co2 and TVOC")
+args = parser.parse_args()
+
+time = []
+co2 = []
+tvoc = []
+
+with open(args.datafile) as csvfile:
+    myreader = csv.reader(csvfile)
+    for row in myreader:
+        time.append(float(row[0]))
+        co2.append(int(row[1]))
+        tvoc.append(int(row[2]))
+
+formatted_time = [matplotlib.dates.epoch2num(timepoint) for timepoint in time]
+
+plt.plot_date(formatted_time, co2, label = "co2")
+plt.plot_date(formatted_time, tvoc, label = "tvoc")
+
+plt.legend()
+plt.xlabel("time")
+plt.ylabel("ppm")
+
+plt.show()
