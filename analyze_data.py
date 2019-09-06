f3f94edc8f850494cb928bf2094e906a5e896dcc
JoeMulka <msutennis23@gmail.com>
2019-09-05 23:58:59 -0400

    fixed x axis labels

--- analyze_data.py
+++ analyze_data.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import matplotlib
 import csv
 import argparse
@@ -20,13 +21,17 @@ with open(args.datafile) as csvfile:
         co2.append(int(row[1]))
         tvoc.append(int(row[2]))
 
-formatted_time = [matplotlib.dates.epoch2num(timepoint) for timepoint in time]
+formatted_time = [mdates.epoch2num(timepoint) for timepoint in time]
 
-plt.plot_date(formatted_time, co2, label = "co2")
-plt.plot_date(formatted_time, tvoc, label = "tvoc")
+myfig = plt.figure()
+ax = myfig.add_subplot(1,1,1)
+ax.plot_date(formatted_time, co2, xdate=True, label = "co2")
+ax.plot_date(formatted_time, tvoc, xdate=True, label = "tvoc")
 
 plt.legend()
 plt.xlabel("time")
 plt.ylabel("ppm")
+date_format = mdates.DateFormatter('%I:%M%p')
+ax.xaxis.set_major_formatter(date_format)
 
 plt.show()
